3e6193d15aadd2d450b172a3cedd2176a8b823d6
Tiago Pereira Ramos <tiagopereiraramos@gmail.com>
2023-09-29 11:42:57 -0400

    fix

--- tasks_methods/methods.py
+++ tasks_methods/methods.py
@@ -75,6 +75,7 @@ class ScraperMethods:
                 options.add_argument("--disable-dev-shm-usage")
                 options.add_argument("--no-sandbox") 
                 options.add_argument("--disable-blink-features=AutomationControlled")
+                options.add_argument("--window-size=1920,1080")
                 # Exclude the collection of enable-automation switches 
                 options.add_experimental_option("excludeSwitches", ["enable-automation"]) 
                 # Turn-off userAutomationExtension 
