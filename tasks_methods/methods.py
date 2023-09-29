0f862dac1129ca4808c85ca270572261385323d3
Tiago Pereira Ramos <tiagopereiraramos@gmail.com>
2023-09-29 09:38:59 -0400

    add options on browser

--- tasks_methods/methods.py
+++ tasks_methods/methods.py
@@ -73,7 +73,8 @@ class ScraperMethods:
                 options = Options()
                 options.add_argument("--headless")
                 options.add_argument("--start-maximized")
-                options.add_argument('--disable-dev-shm-usage')
+                options.add_argument("--disable-dev-shm-usage")
+                options.add_argument("--no-sandbox") 
                 driver.open_browser(
                     url=site_url,
                     browser='headlesschrome',
